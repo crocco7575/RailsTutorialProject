cd0ab49299c704e974c585d7db6a43b48cf16751
Cameron Rocco <cameron@annarbortees.com>
2026-05-15 16:04:14 -0400

    (SOF-2765) Did Part 2 of the Rails Tutorial

--- config/routes.rb
+++ config/routes.rb
@@ -1,35 +1,35 @@
 Rails.application.routes.draw do
   resource :session
   resources :passwords, param: :token
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-
-  # Defines the root path route ("/")
-  # root "posts#index"
 
   resources :products do
     resources :subscribers, only: [ :create ]
   end
   resource :unsubscribe, only: [ :show ]
+  resource :sign_up, only: [ :show, :create ]
+
   root "products#index"
 
-  # get "/products", to: "products#index", as: :products
+  namespace :settings do
+    resource :password, only: [ :show, :update ]
+    resource :profile, only: [ :show, :update ]
+    resource :user, only: [ :show, :destroy ]
+    resource :email, only: [ :show, :update ]
 
-  # get "/products/new", to: "products#new", as: :new_product
-  # post "/products", to: "products#create"
+    root to: redirect("/settings/profile")
+  end
 
-  # get "/products/:id", to: "products#show", as: :product
+  namespace :email do
+    resources :confirmations, param: :token, only: [ :show ]
+  end
 
-  # get "/products/:id/edit", to: "products#edit", as: :edit_product
-  # patch "/products/:id", to: "products#update"
-  # put "/products/:id", to: "products#update"
+    # Admins Only
+  namespace :store do
+    resources :products
+    resources :users
 
-  # delete "/products/:id", to: "products#destroy"
+    root to: redirect("/store/products")
+  end
+
+
 end
